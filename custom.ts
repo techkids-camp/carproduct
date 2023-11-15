34a769ad7f3fd886e8ae089e7b0b9e2726e25c6d
shu_999 <syu.inui@gmail.com>
2023-11-15 23:22:20 +0900

    道路

--- custom.ts
+++ custom.ts
@@ -30,6 +30,80 @@ enum halfBlocks {
     //% block="たけの木"
     BAMBOO
 }
+enum paintblocks{
+    //% block="きいろのカーペット"
+    YELLOW_CARPET=262315,
+    //% block="しろのカーペット"
+    WHITE_CARPET=171
+}
+
+//% color=#696969 weight=400 icon="\uf018" block=みちをつくる
+namespace road {
+    //% blockId=makeLigthPaint
+    //% block="みぎのまんなかのてんせんを $block でつくる"
+    //% block.shadow=minecraftBlocks
+    export function makeLigthPaint(block:paintblocks){
+        agent.teleport(world(-1350, 69, -1264), WEST)
+        agent.move(FORWARD, 5)
+        agent.setItem(block, 64, 1)
+        for (let index = 0; index < 7; index++) {
+            for (let index = 0; index < 5; index++) {
+                agent.place(DOWN)
+                agent.move(FORWARD, 1)
+            }
+            agent.move(FORWARD, 5)
+        }
+        for (let index = 0; index < 5; index++) {
+            agent.place(DOWN)
+            agent.move(FORWARD, 1)
+        }
+    }
+    //% blockId=makeLeftPaint
+    //% block="ひだりのまんなかのてんせんを $block でつくる"
+    //% block.shadow=minecraftBlocks
+    export function makeLeftPaint(block:paintblocks){
+        agent.teleport(world(-1350, 69, -1272), WEST)
+        agent.move(FORWARD, 5)
+        agent.setItem(block, 64, 1)
+        for (let index = 0; index < 7; index++) {
+            for (let index = 0; index < 5; index++) {
+                agent.place(DOWN)
+                agent.move(FORWARD, 1)
+            }
+            agent.move(FORWARD, 5)
+        }
+        for (let index = 0; index < 5; index++) {
+            agent.place(DOWN)
+            agent.move(FORWARD, 1)
+        }
+    }
+    //% blockId=makeMiddlePaint
+    //% block="まんなかのせんを $block でつくる"
+    //% block.shadow=minecraftBlocks
+    export function makeMiddlePaint(block: paintblocks) {
+        agent.teleport(world(-1351, 69, -1268), WEST)
+        agent.setItem(block, 64, 1)
+        for (let index = 0; index < 80; index++) {
+            agent.place(DOWN)
+            agent.move(FORWARD, 1)
+        }
+    }
+    //% blockId=makeroad
+    //% block="どうろをいしでつくる"
+    //% block.shadow=minecraftBlock
+    export function makeroad() {
+        player.execute(
+            "/fill -1351 67 -1275 -1430 67 -1261 stone"
+        )
+    }
+    //% blokId=reset
+    //% block="さいしょにもどす"
+    export function reset(){
+        player.execute(
+            "/fill -1351 67 -1275 -1430 68 -1261 air"
+        )
+    }
+}
 
 /**
  * Custom blocks
