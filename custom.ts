031fb9692453823192c335a6b39b6a1de7b28c0f
shu_999 <syu.inui@gmail.com>
2023-11-17 14:58:41 +0900

    道路の変更

--- custom.ts
+++ custom.ts
@@ -6,35 +6,33 @@
 
 enum halfBlocks {
     //% block="なめらかないし"
-    SMOOTH_STONE,
+    SMOOTH_STONE = 43,
     //% block="いし"
-    STONE,
+    STONE = 131978,
     //% block="まるいし"
-    COLLABLE_STONE,
+    COLLABLE_STONE = 196652,
     //% block="オークの木"
-    OAK,
+    OAK = 158,
     //% block="トウヒの木"
-    SPRUCE,
+    SPRUCE = 65694,
     //% block="しらかばの木"
-    BIRCH,
+    BIRCH = 131230,
     //% block="ジャングルの木"
-    JUNGLE,
+    JUNGLE = 196766,
     //% block="アカシアの木"
-    ACACIA,
+    ACACIA = 262302,
     //% block="ダークオークの木"
-    DARK_OAK,
+    DARK_OAK = 327838,
     //% block="マングローブの木"
-    MANGROVE,
+    MANGROVE = 839,
     //% block="さくらの木"
-    CHERRY,
+    CHERRY = 928,
     //% block="たけの木"
-    BAMBOO
+    BAMBOO = 912
 }
 enum paintblocks{
-    //% block="きいろのカーペット"
-    YELLOW_CARPET=262315,
-    //% block="しろのカーペット"
-    WHITE_CARPET=171
+    //% block="クォーツのハーフブロック"
+    STONE_SLAB = 393260
 }
 
 //% color=#696969 weight=400 icon="\uf018" block=みちをつくる
@@ -45,19 +43,16 @@ namespace road {
     //% block="みぎのまんなかのてんせんを $block でつくる"
     //% block.shadow=minecraftBlocks
     export function makeLigthPaint(block:paintblocks){
-        agent.teleport(world(-1350, 69, -1264), WEST)
-        agent.move(FORWARD, 5)
+        agent.teleport(world(-2713, 65, -124), WEST)
+        agent.move(FORWARD, 3)
         agent.setItem(block, 64, 1)
-        for (let index = 0; index < 7; index++) {
+        for (let index = 0; index < 5; index++) {
             for (let index = 0; index < 5; index++) {
+                agent.destroy(DOWN)
                 agent.place(DOWN)
                 agent.move(FORWARD, 1)
             }
             agent.move(FORWARD, 5)
-        }
-        for (let index = 0; index < 5; index++) {
-            agent.place(DOWN)
-            agent.move(FORWARD, 1)
         }
 
         roadAnswerCheckList.push(2);
@@ -67,19 +62,16 @@ namespace road {
     //% block="ひだりのまんなかのてんせんを $block でつくる"
     //% block.shadow=minecraftBlocks
     export function makeLeftPaint(block:paintblocks){
-        agent.teleport(world(-1350, 69, -1272), WEST)
-        agent.move(FORWARD, 5)
+        agent.teleport(world(-2713, 65, -132), WEST)
+        agent.move(FORWARD, 3)
         agent.setItem(block, 64, 1)
-        for (let index = 0; index < 7; index++) {
+        for (let index = 0; index < 5; index++) {
             for (let index = 0; index < 5; index++) {
+                agent.destroy(DOWN)
                 agent.place(DOWN)
                 agent.move(FORWARD, 1)
             }
             agent.move(FORWARD, 5)
-        }
-        for (let index = 0; index < 5; index++) {
-            agent.place(DOWN)
-            agent.move(FORWARD, 1)
         }
 
         roadAnswerCheckList.push(3);
@@ -89,9 +81,10 @@ namespace road {
     //% block="まんなかのせんを $block でつくる"
     //% block.shadow=minecraftBlocks
     export function makeMiddlePaint(block: paintblocks) {
-        agent.teleport(world(-1351, 69, -1268), WEST)
+        agent.teleport(world(-2713, 65, -128), WEST)
         agent.setItem(block, 64, 1)
-        for (let index = 0; index < 80; index++) {
+        for (let index = 0; index < 51; index++) {
+            agent.destroy(DOWN)
             agent.place(DOWN)
             agent.move(FORWARD, 1)
         }
@@ -104,7 +97,7 @@ namespace road {
     //% block.shadow=minecraftBlock
     export function makeroad() {
         player.execute(
-            "/fill -1351 67 -1275 -1430 67 -1261 stone"
+            "/fill -2713 64 -135 -2763 64 -121 stone_block_slab4 2"
         )
 
         roadAnswerCheckList.push(1);
@@ -114,7 +107,7 @@ namespace road {
     //% block="さいしょにもどす"
     export function reset(){
         player.execute(
-            "/fill -1351 67 -1275 -1430 68 -1261 air"
+            "/fill -2713 64 -135 -2763 68 -121 air"
         )
 
         roadAnswerCheckList = [];
@@ -153,6 +146,8 @@ namespace road {
  */
 //% color=#B40404 weight=400 icon="\uf1b9" block=くるまをつくる
 namespace custom {
+    let checkList: number[] = [];
+
     /**
      * TODO: describe your function here
      * @param value describe value here, eg: 5
@@ -160,7 +155,13 @@ namespace custom {
     //% blockID=makeCircle
     //% block="しゃたい(下)を $myBlock でつくる"
     export function makeBottomBody(myBlock: halfBlocks) {
-        buildingCarBody(myBlock);
+        agent.setSlot(1)
+        agent.setItem(myBlock, 64, 1);
+        buildingCarBottom(myBlock);
+
+        checkList.push(1);
+
+        answerCheck();
     }
 
     /**
@@ -171,20 +172,298 @@ namespace custom {
     //% block="しゃたいを $myBlock でつくる"
     //% myBlock.shadow=minecraftBlock
     export function makeBody(myBlock: number) {
+        agent.setSlot(1)
+        agent.setItem(myBlock, 64, 1);
         buildingCarBody(myBlock);
+
+        checkList.push(2);
+
+        answerCheck();
     }
 
     /**
      * TODO: describe your function here
-     * @param value describe value here, eg: 5
+     * @param value describe value here, eg: 1
      */
     //% blockID=makeCircle
     //% block="しゃたい(上)を $myBlock でつくる"
     export function makeUpBody(myBlock: halfBlocks) {
-        buildingCarBody(myBlock);
+        agent.setSlot(1);
+        agent.setItem(myBlock, 64, 1);
+        buildingCarUp(myBlock);
+
+        checkList.push(3);
+
+        answerCheck();
     }
 
-    function buildingCarBody(value: halfBlocks){
+    /**
+     * TODO: describe your function here
+     */
+    //% blockID=makeCircle
+    //% block="まどガラスをつくる"
+    export function makeGlass() {
+        agent.setSlot(1);
+        buildingCarGlass();
+
+        checkList.push(4);
+
+        answerCheck();
+    }
+
+    /**
+     * TODO: describe your function here
+     */
+    //% blockID=makeCircle
+    //% block="ドアをつくる"
+    export function makeDoor() {
+        agent.setSlot(1);
+        buildingCarDoor();
+
+        checkList.push(6);
+
+        answerCheck();
+    }
+
+    /**
+     * TODO: describe your function here
+     */
+    //% blockID=makeCircle
+    //% block="タイヤをつくる"
+    export function makeWheel() {
+        agent.setSlot(1);
+        buildingCarWheel();
+
+        checkList.push(5);
+
+        answerCheck();
+    }
+
+    function buildingCarBottom(value: number){
+        agent.setItem(STONE, 64, 2)
+        for (let index = 0; index < 9; index++) {
+            for (let index = 0; index < 4; index++) {
+                agent.destroy(DOWN)
+                agent.move(RIGHT, 1)
+            }
+            agent.move(FORWARD, 1)
+            agent.move(LEFT, 4)
+        }
+        agent.move(BACK, 1)
+        agent.move(DOWN, 1)
+        agent.move(LEFT, 4)
+        for (let index = 0; index < 9; index++) {
+            for (let index = 0; index < 3; index++) {
+                agent.move(UP, 1)
+                agent.setSlot(2)
+                agent.place(UP)
+                agent.move(DOWN, 1)
+                agent.setSlot(1)
+                agent.place(UP)
+                agent.move(RIGHT, 1)
+            }
+            agent.move(UP, 1)
+            agent.setSlot(2)
+            agent.place(UP)
+            agent.move(DOWN, 1)
+            agent.setSlot(1)
+            agent.place(UP)
+            agent.move(BACK, 1)
+            agent.move(LEFT, 3)
+        }
+        agent.destroy(BACK)
+        agent.move(FORWARD, 7)
+        agent.setItem(DIRT, 64, 2)
+        agent.setSlot(2)
+        for (let index = 0; index < 8; index++) {
+            for (let index = 0; index < 3; index++) {
+                agent.place(FORWARD)
+                agent.move(RIGHT, 1)
+            }
+            agent.place(FORWARD)
+            agent.move(BACK, 1)
+            agent.move(LEFT, 3)
+        }
+        agent.move(RIGHT, 2)
+        agent.place(RIGHT)
+        agent.move(LEFT, 1)
+        agent.place(RIGHT)
+        agent.move(LEFT, 1)
+        agent.place(RIGHT)
+        agent.move(BACK, 1)
+        agent.place(FORWARD)
+        agent.move(UP, 1)
+        agent.place(DOWN)
+        agent.move(UP, 1)
+        for (let index = 0; index < 9; index++) {
+            for (let index = 0; index < 3; index++) {
+                agent.destroy(FORWARD)
+                agent.move(RIGHT, 1)
+            }
+            agent.destroy(FORWARD)
+            agent.move(FORWARD, 1)
+            agent.move(LEFT, 3)
+        }
+        agent.move(BACK, 1)
+        agent.destroy(DOWN)
+        agent.move(RIGHT, 3)
+        agent.destroy(DOWN)
+        agent.move(BACK, 6)
+        agent.destroy(DOWN)
+        agent.move(LEFT, 3)
+        agent.destroy(DOWN)
+    }
+
+    function buildingCarBody(value: number){
+        const ableBuild: number[][][] = [
+            [
+                [1, 1, 1, 1],
+                [1, 0, 0, 1],
+                [1, 0, 0, 1],
+                [0, 0, 0, 0],
+                [1, 0, 0, 1],
+                [0, 0, 0, 0],
+                [1, 0, 0, 1],
+                [1, 1, 1, 1],
+                [1, 1, 1, 1],
+            ],
+            [
+                [1, 0, 0, 1],
+                [0, 0, 0, 0],
+                [1, 0, 0, 1],
+                [0, 0, 0, 0],
+                [1, 0, 0, 1],
+                [0, 0, 0, 0],
+                [1, 0, 0, 1],
+            ]
+        ]
+
+        agent.move(FORWARD, 6);
+        ableBuild.forEach((oneL, i) => {
+            oneL.forEach((oneL2, j) => {
+                oneL2.forEach((one, k) => {
+                    if (one === 1) agent.place(FORWARD);
+                    agent.move(RIGHT, 1);
+                })
+                agent.move(BACK, 1);
+                agent.move(LEFT, 4);
+            });
+            agent.move(UP, 1);
+            agent.move(FORWARD, 9);
+        })
+    }
+
+    function buildingCarUp(value: halfBlocks){
+        agent.move(BACK, 2);
+        for(let i = 0; i < 6; i++){
+            for (let j = 0; j < 4; j++){
+                agent.place(FORWARD);
+                agent.move(RIGHT, 1);
+            }
+            agent.move(BACK, 1);
+            agent.move(LEFT, 4);
+        }
+
+        agent.move(BACK, 2);
+        agent.move(DOWN, 2);
+
+        agent.setItem(FRAME, 64, 2)
+        agent.setItem(LADDER, 64, 3)
+
+        agent.setSlot(2);
+        agent.place(FORWARD);
+
+        agent.setSlot(3);
+        agent.move(RIGHT, 1);
+        agent.place(FORWARD);
+        agent.move(RIGHT, 1);
+        agent.place(FORWARD);
+        agent.move(RIGHT, 1);
+
+        agent.setSlot(2);
+        agent.place(FORWARD);
+
+        agent.move(UP, 3);
+
+        agent.move(FORWARD, 10);
+        agent.turn(LEFT);
+        agent.turn(LEFT);
+        agent.move(DOWN, 3);
+
+        agent.place(FORWARD);
+        agent.move(RIGHT, 3);
+        agent.place(FORWARD);
+    }
+
+    function buildingCarGlass(){
+        agent.setItem(GLASS, 64, 1)
+        agent.move(LEFT, 1)
+        agent.move(UP, 1)
+        agent.place(FORWARD)
+        agent.move(LEFT, 1)
+        agent.place(FORWARD)
+        agent.move(LEFT, 2)
+        agent.move(FORWARD, 2)
+        agent.place(RIGHT)
+        agent.move(FORWARD, 6)
+        agent.move(RIGHT, 2)
+        agent.place(BACK)
+        agent.move(RIGHT, 1)
+        agent.place(BACK)
+        agent.move(RIGHT, 2)
+        agent.move(BACK, 6)
+        agent.place(LEFT)
+    }
+
+    function buildingCarWheel(){
+        agent.setItem(BLACK_CONCRETE, 64, 1)
+        agent.move(DOWN, 2)
+        agent.place(LEFT)
+        agent.move(FORWARD, 6)
+        agent.place(LEFT)
+        agent.move(UP, 2)
+        agent.move(LEFT, 5)
+        agent.move(DOWN, 2)
+        agent.place(RIGHT)
+        agent.move(BACK, 6)
+        agent.place(RIGHT)
+    }
+
+    function buildingCarDoor(){
+        agent.setItem(IRON_DOOR, 64, 1)
+        agent.move(FORWARD, 2)
+        agent.move(UP, 1)
+        agent.turn(RIGHT_TURN)
+        agent.place(FORWARD)
+        agent.move(LEFT, 2)
+        agent.place(FORWARD)
+        agent.move(UP, 3)
+        agent.move(FORWARD, 5)
+        agent.move(DOWN, 3)
+        agent.turn(RIGHT_TURN)
+        agent.turn(RIGHT_TURN)
+        agent.place(FORWARD)
+        agent.move(LEFT, 2)
+        agent.place(FORWARD)
+    }
+
+    function answerCheck(){
+        if(checkList.length !== 6) return;
+
+        const answerList = [1,2,3,4,5,6];
         
+        for(let i = 0; i < answerList.length; i++){
+            if(answerList[i] !== checkList[i]){
+                // とりあえず仮(本番はダイアログ)
+                player.say("プログラムがちがうよ！");
+                checkList = [];
+                return;
+            }
+        }
+
+        checkList = [];
+
+        // 正解処理
+        player.execute('scoreboard players add "くるまのかず" car 1');
     }
 }
